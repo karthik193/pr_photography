0f19a1ccbd17ea30417151b252641f382e1bb23b
karthik <karthik.pasupulatei@gmail.com>
2021-10-10 19:55:50 +0530

    structure defined

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
-
+import info from './dataReference/info' ; 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App"  align = "center">
+      {info.title} <br/>
+      My name is {info.admin.name}<br/>
+      {/* <img src = {process.env.PUBLIC_URL + "/Images/logo.png"}  alt  = "NO IMAGGE"  /> */}
     </div>
   );
 }
 
 export default App;
+
+
+// complete about.js, navbar.js 
---
+++ src/dataReference/info.js
@@ -0,0 +1,11 @@
+export default {
+    "title" : "PR Photography", 
+    "admin" : {
+        "name" : "Rohit Podila", 
+        "age" : 26 , 
+        "contact" : ["+917093123347" , "+919492393347"], 
+        "address" : "16-1-192, near Vijay Durga Temple, Saidabad, Hyderabad, Telangana,500059."
+    },
+    "about" : "Here comes the Description" ,
+    "categories" : ["Traditional" , "Nature" , "nextCatog" ] 
+}
