56e6a52f8028c5eacc8659434dcd83b1ad72fdb8
karthik <karthik.pasupulatei@gmail.com>
2021-10-10 20:08:18 +0530

    structuredefined & files added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
+import './style/about.css'
 import info from './dataReference/info' ; 
 function App() {
   return (
     <div className="App"  align = "center">
-      {info.title} <br/>
+      <h1>{info.title}</h1> <br/>
       My name is {info.admin.name}<br/>
-      {/* <img src = {process.env.PUBLIC_URL + "/Images/logo.png"}  alt  = "NO IMAGGE"  /> */}
+      <img src = {process.env.PUBLIC_URL + "/Images/logo.png"}  alt  = "NO IMAGGE"  />
     </div>
   );
 }
