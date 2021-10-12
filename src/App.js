1cf4135e7db1b14baf397aca89a532e609b99e6a
Karthik Pasupulatei <karthik.pasupulatei@gmail.com>
2021-10-12 19:43:37 +0530

    yesterdasy

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 import './style/about.css'
 import info from './dataReference/info' ; 
+
+import ImageGrid from "./components/imageGrid" ; 
 function App() {
   return (
     <div className="App"  align = "center">
       <h1>{info.title}</h1> <br/>
       My name is {info.admin.name}<br/>
       <img src = {process.env.PUBLIC_URL + "/Images/logo.png"}  alt  = "NO IMAGGE"  />
+      <ImageGrid></ImageGrid>
     </div>
   );
 }
---
+++ src/components/imageGrid.js
@@ -0,0 +1,16 @@
+import React from "react" ;
+
+
+export default function ImageGrid(props){
+
+
+    return(
+        <div className = "row">
+            <div className = "col-md-4" ></div>
+            <div className = "col-md-4"></div>
+            <div className = "col-md-4" ></div>
+        </div>
+    )
+}
+
+
