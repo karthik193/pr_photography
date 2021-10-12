8c2ed4c0b6060561b3d306c7b034ee43e6e9803d
TarangGarlapally <taranggarlapally@gmail.com>
2021-10-12 12:29:34 +0530

    nav update and about update

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import './App.css';
 import './style/about.css';
 import Banner from "./components/Banner";
 import Navbar from './components/Navbar';
+import About from './components/About';
+import {Switch, Route} from "react"
 
 function App() {
   return (
     <div className="App">
       <Banner />
       <Navbar />
-    </div>
+      <About />
+  </div>
   );
 }
 
