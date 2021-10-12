35c327df4d2757c8462b7d33c2c1b296f4deb6de
Karthik Pasupulatei <karthik.pasupulatei@gmail.com>
2021-10-12 20:30:51 +0530

    actual Images added

--- src/components/imageGrid.jsx
+++ src/components/imageGrid.jsx
@@ -7,17 +7,23 @@ export default function ImageGrid() {
         <div  className  = "row" align = "center">
             <div className = "col">
                 <img className  = "gridImage" src = {process.env.PUBLIC_URL + "/Images/temp/1.jpg"} ></img>
-                <img className  = "gridImage" src = {process.env.PUBLIC_URL + "/Images/temp/2.webp"} ></img>
+                <img className  = "gridImage" src = {process.env.PUBLIC_URL + "/Images/temp/2.jpg"} ></img>
+                <img className  = "gridImage" src = {process.env.PUBLIC_URL + "/Images/temp/6.jpg"} ></img>
+                <img className  = "gridImage" src = {process.env.PUBLIC_URL + "/Images/temp/10.jpg"} ></img>
                 
 
             </div>
             <div className = "col">
                 <img className  = "gridImage" src = {process.env.PUBLIC_URL + "/Images/temp/3.jpg"} ></img>
+                <img className  = "gridImage" src = {process.env.PUBLIC_URL + "/Images/temp/7.jpg"} ></img>
+                <img className  = "gridImage" src = {process.env.PUBLIC_URL + "/Images/temp/8.jpg"} ></img>
                 <img className  = "gridImage" src = {process.env.PUBLIC_URL + "/Images/temp/4.jpg"} ></img>
             </div>
             <div className = "col">
                 <img className  = "gridImage" src = {process.env.PUBLIC_URL + "/Images/temp/5.jpg"} ></img>
                 <img className  = "gridImage" src = {process.env.PUBLIC_URL + "/Images/temp/3.jpg"} ></img>
+                <img className  = "gridImage" src = {process.env.PUBLIC_URL + "/Images/temp/11.jpg"} ></img>
+                <img className  = "gridImage" src = {process.env.PUBLIC_URL + "/Images/temp/9.jpg"} ></img>
             </div>
         </div>
     );
