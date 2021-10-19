475ecbea9e263a783bd786905c08e247f375d529
TarangGarlapally <taranggarlapally@gmail.com>
2021-10-19 12:05:23 +0530

    added collapsible for categories

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,6 +3,7 @@ import React, {useState} from "react";
 import { useHistory } from "react-router";
 import { redirectHandler } from "../functions/helpers";
 import "../style/navbar.css";
+import Collapsible from 'react-collapsible';
 
 
 export default function Navbar(props){
@@ -13,6 +14,11 @@ export default function Navbar(props){
     <div className = "sideNav">
         <p onClick = {() => {redirectHandler(history , "/about")}}>ABOUT</p>
         <p>CATEGORIES</p>
+        <Collapsible trigger="CATEGORIES" className = "collapsible">
+            <p>Category1</p>
+            <p>Category2</p>
+            <p>Category3</p>
+        </Collapsible>
         <p onClick = {() => {redirectHandler(history ,"/contact")}}>CONTACT</p>
     </div></div>;
 }
