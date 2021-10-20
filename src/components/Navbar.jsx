1600728006f0af660eb7d8cd32b1dc2c3ba8fcbd
TarangGarlapally <taranggarlapally@gmail.com>
2021-10-20 12:32:47 +0530

    added sidebar for mobile view

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,29 +5,82 @@ import { redirectHandler } from "../functions/helpers";
 import "../style/navbar.css";
 import Collapsible from 'react-collapsible';
 import {getFirestore,doc, getDoc} from 'firebase/firestore' ; 
+import SwipeableDrawer from '@material-ui/core/SwipeableDrawer';
+
 
 
 export default function Navbar(props){
     const history = useHistory(); 
     const path = useState("");
-    const [catList, setCatList] = useState([]);
+    const [catListFlag, setCatListFlag] = useState(false);
     const Upload  = <p onClick = {() => {redirectHandler(history , "/upload")}}>UPLOAD</p> ; 
+    const [sidebar, setSidebar] = useState(false);
+    var catList = [];
+
+
+    
+    const toggleDrawer = (flag) => (event) => {
+        if (
+          event &&
+          event.type === 'keydown' &&
+          (event.key === 'Tab' || event.key === 'Shift')
+        ) {
+          return;
+        }
+
+        setSidebar(flag)
+    };
+
+
+const styles = {
+  BackdropProps: {
+    background: 'transparent'
+  }
+};
 
 
     useEffect(async ()=>{
         const db = getFirestore();
         const docRef = doc(db, "meta-info", "categories");
         const docSnap = await getDoc(docRef);
-        setCatList(docSnap.data().categoryList);
-    });
+        console.log("test")
+        catList = docSnap.data().categoryList;
+        setCatListFlag(true);
+    }, [catListFlag]);
 
-    return  <div><img className="logoImg" align = "left" src = {process.env.PUBLIC_URL + "/Images/logo.png"}  onClick = {() => {redirectHandler(history , "/")}}alt  = "Logo"  />
-    <div className = "sideNav" id = "sideNav">
-        {localStorage.getItem("admin") == "true" ? Upload : null}
-        <p onClick = {() => {redirectHandler(history , "/about")}}>ABOUT</p>
-        <Collapsible trigger="CATEGORIES" className = "collapsible" onOpening = {() => {document.getElementById("sideNav").className = "sideNav collapse"}} onClose = {() => {document.getElementById("sideNav").className = "sideNav"}}>
-            {catList.map((cat, index) => <p key = {index}>{cat}</p>)}
-        </Collapsible>
-        <p onClick = {() => {redirectHandler(history ,"/contact")}}>CONTACT</p>
-    </div></div>;
+    return  <div>
+        <div className="navbar">
+            <img className="logoImg" align = "left" src = {process.env.PUBLIC_URL + "/Images/logo.png"}  onClick = {() => {redirectHandler(history , "/")}}alt  = "Logo"  />
+            <div className = "sideNav" id = "sideNav">
+                {localStorage.getItem("admin") == "true" ? Upload : null}
+                <p onClick = {() => {redirectHandler(history , "/about")}}>ABOUT</p>
+                <Collapsible trigger="CATEGORIES" className = "collapsible" onOpening = {() => {document.getElementById("sideNav").className = "sideNav collapse"}} onClose = {() => {document.getElementById("sideNav").className = "sideNav"}}>
+                    {catList.map((cat, index) => <p key = {index}>{cat}</p>)}
+                </Collapsible>
+                <p onClick = {() => {redirectHandler(history ,"/contact")}}>CONTACT</p>
+            </div>
+        </div>
+
+    <div className = "drawer">
+        <button onClick={toggleDrawer(true)}>click</button>
+        <SwipeableDrawer
+                anchor="left"
+                open={sidebar}
+                onClose={toggleDrawer(false)}
+                onOpen={toggleDrawer(true)}
+                          >
+              <div className="swipeableIn">
+                  <h1>Hielel</h1>
+                    <div className = "sideNav" id = "sideNav">
+                        {localStorage.getItem("admin") == "true" ? Upload : null}
+                        <p onClick = {() => {redirectHandler(history , "/about")}}>ABOUT</p>
+                        <Collapsible trigger="CATEGORIES" className = "collapsible" onOpening = {() => {document.getElementById("sideNav").className = "sideNav collapse"}} onClose = {() => {document.getElementById("sideNav").className = "sideNav"}}>
+                            {catList.map((cat, index) => <p key = {index}>{cat}</p>)}
+                        </Collapsible>
+                        <p onClick = {() => {redirectHandler(history ,"/contact")}}>CONTACT</p>
+                    </div>
+              </div>
+        </SwipeableDrawer>
+    </div>
+    </div>;
 }
